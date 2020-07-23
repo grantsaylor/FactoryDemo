aa798c3c0e6dbeb5a01a281c6126a75d195043df
grant saylor <grantsaylor@outlook.com>
2020-07-22 17:55:09 -0700

    Need to implement factory

--- src/part1/game/character/CharacterFactory.java
+++ src/part1/game/character/CharacterFactory.java
@@ -2,15 +2,31 @@ package part1.game.character;
 
 public class CharacterFactory {
 
-    public Character createCharacter(String info){
-        if(info.contentEquals("standing")){
+    public Character createCharacter(String info) {
+        if (info.contentEquals("standing")) {
             return new StandingCharacter();
-        }else if(info.contentEquals("sitting")){
+        } else if (info.contentEquals("sitting")) {
             return new SittingCharacter();
+        } else if (info.contentEquals("leftcrabwalking")) {
+            return new LeftCrabwalkingCharacter();
+        } else if (info.contentEquals("rightcrabwalking")) {
+            return new RightCrabwalkingCharacter();
+        } else if (info.contentEquals("leftwalking")) {
+            return new LeftWalkingCharacter();
+        } else if (info.contentEquals("rightwalking")) {
+            return new RightWalkingCharacter();
+        } else if (info.contentEquals("leftrunning")) {
+            return new LeftRunningCharacter();
+        } else if (info.contentEquals("rightrunning")) {
+            return new RightRunningCharacter();
+        } else if (info.contentEquals("leftcrawling")) {
+            return new LeftCrawlingCharacter();
+        } else if (info.contentEquals("rightcrawling")) {
+            return new RightCrawlingCharacter();
+        } else if (info.contentEquals("jumping")) {
+            return new JumpingCharacter();
         }
-
         return null;
-
     }
 
 }
